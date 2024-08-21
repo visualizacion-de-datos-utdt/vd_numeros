fe11bf8c1ecaf320669297f75ef8ae2c53074bbb
Carlos Araujo <infocarlosaraujo@gmail.com>
2024-08-21 00:03:02 -0300

    Refactor code: Add IsotypeBars component for displaying isotipos in a bar chart

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,9 @@
   import IrregularPath from "./lib/IrregularPath.svelte"
   import CoffeColumns from "./lib/CoffeColumns.svelte"
   import Donas from "./lib/Donas.svelte"
+  import IsotypeBars from "./lib/IsotypeBars.svelte"
 
   let numbers = [5, 18, 29, 75, 100]
-  let numbersDivided5 = numbers.map(n => Math.floor(n / 5))
 
   function altura(n) {
     let scale = d3
@@ -18,19 +18,16 @@
 </script>
 
 <main class="container">
-  <!-- ISOTYPE - Canal: altura -->
+  <!-- Isotipos (manual) -->
   <h3 class="headline">Magnitudes. Canal: longitud</h3>
   <div class="iso__container">
     <img style="height: 75px" src="./images/person-iso.svg" alt="person" />
     <img style="height: 150px" src="./images/person-iso.svg" alt="person" />
   </div>
+  <br />
   <hr />
 
-  <!-- Cafés -->
-  <CoffeColumns numbers={numbers} title="Canal: altura. Tipo: columnas" />
-  <hr />
-
-  <!-- Canal: altura -->
+  <!-- Isotipos (array) -->
   <h3 class="headline">Magnitudes. Canal: longitud</h3>
   <div class="iso__container-big">
     {#each numbers as n}
@@ -43,26 +40,15 @@
   </div>
   <hr />
 
-  <IrregularPath title="Canal: longitud (path irregular)" numbers={numbers} />
+  <!-- Cafés -->
+  <CoffeColumns numbers={numbers} title="Canal: altura. Tipo: columnas" />
   <hr />
 
-  <!-- Canal: longitud. Type: Isotype Chart  -->
-  <h3 class="headline">Magnitudes. Canal: longitud. Tipo: gráfico Isotype</h3>
-
-  <div class="iso__container-iso">
-    {#each numbersDivided5 as n, index}
-      <div class="row">
-        {#each Array(n) as m}
-          <img
-            style="height: 50px; padding: 2px"
-            src="./images/person.png"
-            alt="person"
-          />
-        {/each}
-        <p>{numbers[index]}</p>
-      </div>
-    {/each}
-  </div>
+  <!-- Barras Isotipos  -->
+  <IsotypeBars
+    numbers={numbers}
+    title="Magnitudes. Canal: longitud. Tipo: gráfico Isotype"
+  />
   <hr />
 
   <!-- Waffles -->
@@ -73,9 +59,15 @@
   <hr />
 
   <!-- Donas -->
-  <Donas numbers={numbers} title="Magnitudes. Canal: ángulo. Tipo: gráfico donas" />
+  <Donas
+    numbers={numbers}
+    title="Magnitudes. Canal: ángulo. Tipo: gráfico donas"
+  />
   <hr />
 
+  <!-- Contornos irregulares -->
+  <IrregularPath title="Canal: longitud (path irregular)" numbers={numbers} />
+  <hr />
 </main>
 
 <style>
@@ -98,16 +90,7 @@
     align-items: end;
     justify-content: space-around;
   }
-  .iso__container-iso {
-    display: flex;
-    flex-direction: column;
-    max-width: 1000px;
-    align-items: start;
-    justify-content: space-around;
-  }
-  .headline {
-    text-align: left;
-    margin-top: 50px;
-    /* border: 1px solid red; */
+  :global(.headline) {
+    margin: 50px 0;
   }
 </style>
---
+++ src/lib/IsotypeBars.svelte
@@ -0,0 +1,32 @@
+<script>
+  import * as d3 from "d3"
+  export let title = ""
+  export let numbers = []
+  let numbersDivided5 = numbers.map(n => Math.floor(n / 5))
+</script>
+
+<h3 class="headline">{title}</h3>
+<div class="iso__container-iso">
+  {#each numbersDivided5 as n, index}
+    <div class="row">
+      {#each Array(n) as m}
+        <img
+          style="height: 50px; padding: 2px"
+          src="./images/person.png"
+          alt="person"
+        />
+      {/each}
+      <p>{numbers[index]}</p>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .iso__container-iso {
+    display: flex;
+    flex-direction: column;
+    max-width: 1000px;
+    align-items: start;
+    justify-content: space-around;
+  }
+</style>
